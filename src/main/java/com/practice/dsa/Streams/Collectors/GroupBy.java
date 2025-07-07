308a0b65cf70057d92ea7962391cd32e307b794a
AvinashJadhav112 <avinash.j@vervetronics.com>
2025-07-07 16:37:50 +0530

    feat(collectors): GroupingBy of elements with collectors utility

--- src/main/java/com/practice/dsa/Streams/Collectors/GroupBy.java
+++ src/main/java/com/practice/dsa/Streams/Collectors/GroupBy.java
@@ -9,5 +9,6 @@ public class GroupBy {
         List<String> nameList = List.of("Akash","Niraj","Nirbhay","Abhay");
         Map<Character, List<String>> nameMap = nameList.stream().collect(Collectors.groupingBy(name -> name.charAt(0)));
         System.out.println("Map Of names"+ " " +nameMap);
+      //  Map Of names {A=[Akash, Abhay], N=[Niraj, Nirbhay]}
     }
 }
