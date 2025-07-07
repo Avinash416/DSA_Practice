1584302c7631e95022a559ccf3e4f3c0d2c676df
AvinashJadhav112 <avinash.j@vervetronics.com>
2025-07-07 12:27:05 +0530

    feat(linkedlist): Create simple linkedlist with collections

---
+++ src/main/java/com/practice/dsa/LinkList/LinkListCollections.java
@@ -0,0 +1,10 @@
+package com.practice.dsa.LinkList;
+
+import java.util.LinkedList;
+
+public class LinkListCollections {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList<>();
+        linkedList.add(5);
+    }
+}
